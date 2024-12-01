0f0ced79e22a1ce71a999f3469313c0d0c55eb7b
wandieinnocent <wandieinnocent2@gmail.com>
2024-12-01 23:02:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-3xl font-bold underline text-center text-purple-600">Hello world!</h1>
+  )
+}
+
+export default App
